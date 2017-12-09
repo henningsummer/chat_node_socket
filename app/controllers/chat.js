a3393bb1df142c0dadc4155e7a8a09635ab54c25
Henning_Summer <henningsummer3@gmail.com>
2017-12-09 04:41:31 -0300

    Inicio socket

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciachat = function(application, req, res) {
+    var dadosForm = req.body;
+    //console.log(dadosForm);
+    req.assert('apelido', 'Nome é obrigatório').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve ter no 3 ou 15 letras').len(3, 15);
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render("index", { validacao: erros });
+        return;
+
+    }
     res.render('chat');
+
 }
